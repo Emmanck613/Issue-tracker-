cb720baa263da40c8121cb68388666d3569c4c66
Emmanuel <emmanguerrero613@gmail.com>
2025-03-19 14:45:05 -0600

    Error handling added

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -5,8 +5,8 @@ import {prisma} from "@/prisma/client";
 
 // Define the shape of the request body
 const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1),    
+    title: z.string().min(1, "Title is required").max(255, "Cannot excced 255 characters"), 
+    description: z.string().min(1, "Description is required").max(255, "Cannot excced 255 characters")
 });  
 
 export async function POST(request: NextRequest) {
@@ -17,7 +17,7 @@ export async function POST(request: NextRequest) {
 
     if(!validation.success) {
         //return a 400 response with the validation errors
-        return NextResponse.json(validation.error.errors, { status: 400 });
+        return NextResponse.json(validation.error.format(), { status: 400 });
     }
 
     //we await the call to get the new issue
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import React from 'react'
-import { TextField, TextArea, Button } from "@radix-ui/themes";
+import { useState } from 'react';
+import { TextField, Callout, Button } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller} from "react-hook-form";
 import axios from "axios";
@@ -19,10 +20,13 @@ const NewIssuePage = () => {
   const { register, control, handleSubmit } = useForm<NewIssueForm>();
   //we use the next.js router to navigate to different pages
   const router = useRouter();
+  //state variable for error handling. With typescript we can define the type of the state variable
+  const [error, setError] = useState<string | null>(null);
    
   const onSubmit = async (data: NewIssueForm) => {
     //we send the form data to the server using axios
     console.log(data);
+    // setError(null);
     try {
       const response = await axios.post('/api/issues', data);
       console.log(response.data);
@@ -30,35 +34,55 @@ const NewIssuePage = () => {
         router.push('/issues');
       }
     } catch (error) {
-      console.error(error);
-      throw new Error('Failed to create new issue');
+      console.log(error);
+      setError('Failed to create new issue');
+    } finally{
+      //we clear the error message after 5 seconds
+      //we pass a function to our setTimeout to clear the error message
+      setTimeout(() => {
+        setError(null);
+      }
+      , 5000);
     }
   }
 
   return (
-    <form 
-      onSubmit={handleSubmit(onSubmit)} 
-      className='max-w-xl mx-auto space-y-3'
-    >
+    <div className='max-w-xl mx-auto '>
 
-        {/* Using the spread operator (...) to add the title prop to our component */}
-        <TextField.Root placeholder="Title" {...register("title")}>
-        </TextField.Root>
-        {/* For our markdown component we cannot use register, instead we use 
-        a controller component to bind the value of the markdown editor 
-        to the form state. 
-        */}
-        <Controller 
-          name='description' 
-          control={control}
-          render={({ field }) => (
-            <SimpleMDE placeholder="Description" {...field}/>
-          )}  
-        />
-        <Button>
-            Submit New Issue
-        </Button>
-    </form>
+      { //render only when we have an error
+        error &&
+        <Callout.Root variant="surface" color='red' className='mb-2 p-2 '>
+          <Callout.Text>
+          {error}
+          </Callout.Text>
+        </Callout.Root>
+
+      }
+      <form 
+        onSubmit={handleSubmit(onSubmit)} 
+        className='space-y-3 p-1'
+      >
+
+          {/* Using the spread operator (...) to add the title prop to our component */}
+          <TextField.Root placeholder="Title" {...register("title")}>
+          </TextField.Root>
+          {/* For our markdown component we cannot use register, instead we use 
+          a controller component to bind the value of the markdown editor 
+          to the form state. 
+          */}
+          <Controller 
+            name='description' 
+            control={control}
+            render={({ field }) => (
+              <SimpleMDE placeholder="Description" {...field}/>
+            )}  
+          />
+          <Button>
+              Submit New Issue
+          </Button>
+      </form>
+    </div>
+
   )
 }
 
